c023acbd1f0d90ec727da73c278f5930a41d02a0
Yong Qu <yongqu@microsoft.com>
2015-07-23 16:17:53 -0700

    FW: FW: remove jsrt interceptor code. We should be using proxy now and the old feature is obselete. FW: remove jsrt interceptor code. We should be using proxy now and the old feature is obselete. IECAT #: 75593 Code Reviewer: kpathak Changelist: 1574353

--- lib/jsrt/jsrtExternalObject.h
+++ lib/jsrt/jsrtExternalObject.h
@@ -4,8 +4,7 @@
 
 #pragma once
 
-// REVIEW: ChakraCore Dependency
-#include "..\..\..\private\bin\Chakra\jsrtprivate.h"
+#include "chakrart.h"
 
 #define BEGIN_INTERCEPTOR(scriptContext) \
     BEGIN_LEAVE_SCRIPT(scriptContext) \
@@ -28,14 +27,14 @@
 class JsrtExternalType sealed : public Js::DynamicType
 {
 public:
-    JsrtExternalType(JsrtExternalType *type) : Js::DynamicType(type), typeDescription(type->typeDescription) {}
-    JsrtExternalType(Js::ScriptContext* scriptContext, JsExternalTypeDescription * typeDescription);
+    JsrtExternalType(JsrtExternalType *type) : Js::DynamicType(type), jsFinalizeCallback(type->jsFinalizeCallback) {}
+    JsrtExternalType(Js::ScriptContext* scriptContext, JsFinalizeCallback finalizeCallback);
 
-    Js::PropertyId GetNameId() const { return ((Js::PropertyRecord *)typeDescription.className)->GetPropertyId(); }
-    JsExternalTypeDescription * GetExternalTypeDescription() { return &typeDescription; }  
+    //Js::PropertyId GetNameId() const { return ((Js::PropertyRecord *)typeDescription.className)->GetPropertyId(); }
+    JsFinalizeCallback GetJsFinalizeCallback() const { return this->jsFinalizeCallback; }
 
 private:
-    JsExternalTypeDescription typeDescription;
+    JsFinalizeCallback jsFinalizeCallback;
 };
 AUTO_REGISTER_RECYCLER_OBJECT_DUMPER(JsrtExternalType, &Js::Type::DumpObjectFunction);
 
@@ -58,55 +57,10 @@ public:
 
     bool HasReadOnlyPropertiesInvisibleToTypeHandler() override { return true; }
 
-    BOOL HasProperty(Js::PropertyId propertyId) override;
-    BOOL GetProperty(Js::Var originalInstance, Js::PropertyId propertyId, Js::Var* value, Js::PropertyValueInfo* info, Js::ScriptContext* requestContext) override;
-    BOOL GetProperty(Js::Var originalInstance, Js::JavascriptString* propertyNameString, Js::Var* value, Js::PropertyValueInfo* info, Js::ScriptContext* requestContext) override;
-    BOOL GetPropertyReference(Js::Var originalInstance, Js::PropertyId propertyId, Js::Var* value, Js::PropertyValueInfo* info, Js::ScriptContext* requestContext) override;
-    BOOL SetProperty(Js::PropertyId propertyId, Js::Var value, Js::PropertyOperationFlags flags, Js::PropertyValueInfo* info) override;
-    BOOL SetProperty(Js::JavascriptString* propertyNameString, Js::Var value, Js::PropertyOperationFlags flags, Js::PropertyValueInfo* info) override;
-    BOOL SetPropertyWithAttributes(Js::PropertyId propertyId, Js::Var value, Js::PropertyAttributes attributes, Js::PropertyValueInfo* info, Js::PropertyOperationFlags flags = Js::PropertyOperation_None, Js::SideEffects possibleSideEffects = Js::SideEffects_Any) override;
-    BOOL InitProperty(Js::PropertyId propertyId, Js::Var value, Js::PropertyOperationFlags flags = Js::PropertyOperation_None, Js::PropertyValueInfo* info = NULL) override;
-    BOOL DeleteProperty(Js::PropertyId propertyId, Js::PropertyOperationFlags flags) override;
-
-    BOOL HasItem(uint32 index) override;
-    BOOL GetItem(Js::Var originalInstance, uint32 index, Js::Var* value, Js::ScriptContext * requestContext) override;
-    BOOL GetItemReference(Js::Var originalInstance, uint32 index, Js::Var* value, Js::ScriptContext * requestContext) override;
-    Js::DescriptorFlags GetItemSetter(uint32 index, Js::Var* setterValue, Js::ScriptContext* requestContext) override;
-    BOOL SetItem(uint32 index, Js::Var value, Js::PropertyOperationFlags flags) override;
-    BOOL DeleteItem(uint32 index, Js::PropertyOperationFlags flags) override;
-
-    BOOL GetEnumerator(BOOL enumNonEnumerable, Js::Var* enumerator, Js::ScriptContext * requestContext, bool preferSnapshotSemantics = true, bool enumSymbols = false) override;
-
-    BOOL IsWritable(Js::PropertyId propertyId) override;
-    BOOL IsConfigurable(Js::PropertyId propertyId) override;
-    BOOL IsEnumerable(Js::PropertyId propertyId) override;
-
-    // TODO: WARNING: slow perf as it calls GetConfigurable/Enumerable/Writable individually.
-    BOOL SetAttributes(Js::PropertyId propertyId, Js::PropertyAttributes attributes) override;
-
-    BOOL SetEnumerable(Js::PropertyId propertyId, BOOL value) override;
-    BOOL SetWritable(Js::PropertyId propertyId, BOOL value) override;
-    BOOL SetConfigurable(Js::PropertyId propertyId, BOOL value) override;
-
-    BOOL GetAccessors(Js::PropertyId propertyId, Js::Var *getter, Js::Var *setter, Js::ScriptContext * requestContext) override;
-    BOOL SetAccessors(Js::PropertyId propertyId, Js::Var getter, Js::Var setter, Js::PropertyOperationFlags flags) override;
-    Js::DescriptorFlags GetSetter(Js::PropertyId propertyId, Js::Var *setterValue, Js::PropertyValueInfo* info, Js::ScriptContext* requestContext) override;
-    Js::DescriptorFlags GetSetter(Js::JavascriptString* propertyNameString, Js::Var *setterValue, Js::PropertyValueInfo* info, Js::ScriptContext* requestContext) override;
-
-    BOOL JsrtExternalObject::Equals(Js::Var other, BOOL* returnResult, Js::ScriptContext * requestContext) override;
-    BOOL JsrtExternalObject::StrictEquals(Js::Var other, BOOL* returnResult, Js::ScriptContext * requestContext) override;
-
-    Js::JavascriptString* GetClassName(Js::ScriptContext * requestContext) override;
-
-    BOOL GetDiagValueString(Js::StringBuilder<ArenaAllocator>* stringBuilder, Js::ScriptContext* requestContext) override;
-    BOOL GetDiagTypeString(Js::StringBuilder<ArenaAllocator>* stringBuilder, Js::ScriptContext* requestContext) override;
-
     Js::DynamicType* DuplicateType() override;
 
     void * GetSlotData() const;
     void SetSlotData(void * data);
-
-    JsEnumeratePropertiesCallback GetEnumerateCallback() { return GetExternalType()->GetExternalTypeDescription()->enumerateCallback; }
 
 #if DBG
     virtual BOOL DbgCanHaveInterceptors() const override { return true; }
