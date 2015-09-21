Penguinwizzard/ChakraCore

commit 8c1114b7dc7af9d02c48e1fa7be42d3c2bd43a9c

[Bugfix:4370613,4500241] HomeObj for method and generator method in object literals  Change Description:
     Methods and generator methods are allowed to have super expressions.
     This changeset relaxs parser restrictions and adds emission of 'SetHomeObj'  to object literal cases.
     Existing method of checking if a function is a class member using homeObj!=nullptr was updated to use functionInfo flag 'ClassMethod'.
     No perf impact.


 Related Work Items:

     Bug 4370613: [Super][Generator] GeneratorMethod can reference SuperProperty in body and param
     Bug 4500241: Method definitions in Object literals should have HomeObject set

Code review: abchatra

lib/Runtime/Library/ScriptFunction.cpp
@@ -333,7 +333,8 @@ namespace Js
 
         ScriptContext * scriptContext = this->GetScriptContext();
         JavascriptLibrary *javascriptLibrary = scriptContext->GetLibrary();
-        bool isClassMethod = this->GetHomeObj() != nullptr;
+        bool isClassMethod = this->GetFunctionInfo()->IsClassMethod();
+
         JavascriptString* prefixString = nullptr;
         uint prefixStringLength = 0;
         const wchar_t* name = L"";

lib/Parser/parse.cpp
@@ -3205,7 +3205,9 @@ ParseNodePtr Parser::ParseMemberGetSet(OpCode nop, LPCOLESTR* ppNameHint)
         flags = fFncOneArg | fFncNoName;
     }
 
-    ParseNodePtr pnodeFnc = ParseFncDecl<buildAST>(flags | fFncMethod | (nop == knopSetMember ? fFncSetter : fFncNoFlgs), *ppNameHint);
+    this->m_parsingSuperRestrictionState = ParsingSuperRestrictionState_SuperPropertyAllowed;
+    ParseNodePtr pnodeFnc = ParseFncDecl<buildAST>(flags | fFncMethod | (nop == knopSetMember ? fFncSetter : fFncNoFlgs), *ppNameHint,
+        /*isSourceElement*/ false, /*needsPIDOnRCurlyScan*/ false, /*resetParsingSuperRestrictionState*/ false);
 
     if (buildAST)
     {
@@ -3449,7 +3451,9 @@ ParseNodePtr Parser::ParseMemberList(LPCOLESTR pNameHint, ulong* pNameHintLength
 
             // Rewind to the PID and parse a function expression.
             m_pscan->SeekTo(atPid);
-            ParseNodePtr pnodeFunc = ParseFncDecl<buildAST>(fncDeclFlags | (isAsyncMethod ? fFncAsync : fFncNoFlgs), pFullNameHint);
+            this->m_parsingSuperRestrictionState = ParsingSuperRestrictionState_SuperPropertyAllowed;
+            ParseNodePtr pnodeFunc = ParseFncDecl<buildAST>(fncDeclFlags | (isAsyncMethod ? fFncAsync : fFncNoFlgs), pFullNameHint,
+                /*isSourceElement*/ false, /*needsPIDOnRCurlyScan*/ false, /*resetParsingSuperRestrictionState*/ false);
 
             if (isAsyncMethod)
             {

lib/Runtime/ByteCode/ByteCodeEmitter.cpp
@@ -6947,7 +6947,8 @@ void EmitMemberNode(ParseNode *memberNode, Js::RegSlot objectLocation, ByteCodeG
     ParseNode *nameNode=memberNode->sxBin.pnode1;
     ParseNode *exprNode=memberNode->sxBin.pnode2;
 
-    bool isClassMember = exprNode->nop == knopFncDecl && exprNode->sxFnc.IsClassMember();
+    bool hasHomeObj = exprNode->nop == knopFncDecl;
+    bool isClassMember = hasHomeObj && exprNode->sxFnc.IsClassMember();
 
     // Moved SetComputedNameVar before LdFld of prototype b\c loading the prototype undeferres the function TypeHanlder
     // which makes this bytecode too late to influence the function.name
@@ -6986,8 +6987,8 @@ void EmitMemberNode(ParseNode *memberNode, Js::RegSlot objectLocation, ByteCodeG
 
         byteCodeGenerator->Writer()->Element(setOp, exprNode->location, objectLocation, nameNode->location, true);
 
-        // Class members need a reference back to the class.
-        if (isClassMember)
+        // Class and object members need a reference back to the class.
+        if (hasHomeObj)
         {
             byteCodeGenerator->Writer()->Reg2(Js::OpCode::SetHomeObj, exprNode->location, objectLocation);
         }
@@ -7057,8 +7058,8 @@ void EmitMemberNode(ParseNode *memberNode, Js::RegSlot objectLocation, ByteCodeG
         byteCodeGenerator->Writer()->Property(setOp, exprNode->location, objectLocation, funcInfo->FindOrAddReferencedPropertyId(propertyId));
     }
 
-    // Class members need a reference back to the class.
-    if (isClassMember)
+    // Class and object members need a reference back to the class.
+    if (hasHomeObj)
     {
         byteCodeGenerator->Writer()->Reg2(Js::OpCode::SetHomeObj, exprNode->location, objectLocation);
     }

commit 5fd531a681c78f0b3e5fca8e11148dfefe1239f2

Fix core test failure introduced in 8c1114b and re-enable the test

lib/Runtime/Library/ScriptFunction.cpp
@@ -333,7 +333,7 @@ namespace Js
 
         ScriptContext * scriptContext = this->GetScriptContext();
         JavascriptLibrary *javascriptLibrary = scriptContext->GetLibrary();
-        bool isClassMethod = this->GetFunctionInfo()->IsClassMethod();
+        bool isClassMethod = this->GetFunctionInfo()->IsClassMethod() || IsClassConstructor();
 
         JavascriptString* prefixString = nullptr;
         uint prefixStringLength = 0;

test/Basics/rlexe.xml
@@ -1,4 +1,4 @@
-﻿<?xml version="1.0" encoding="utf-8"?>
+<?xml version="1.0" encoding="utf-8"?>
 <regress-exe>
   <test>
     <default>
@@ -7,13 +7,12 @@
       <compile-flags>-Intl-</compile-flags>
     </default>
   </test>
-  <!--
   <test>
     <default>
       <files>ie12ScriptFunctionToStrings.js</files>
       <compile-flags>-ES6Generators -ES6Classes -args summary -endargs</compile-flags>
     </default>
-  </test>-->
+  </test>
   <test>
     <default>
       <files>DomProperties.js</files>