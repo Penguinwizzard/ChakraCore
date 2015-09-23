a1fdd0ea8af599e23af302c3a927f59f8ffef3d3
Farzon Lotfi <falotfi@microsoft.com>
2015-09-23 10:19:00 -0700

    initial work on new.target

--- lib/Parser/parse.cpp
+++ lib/Parser/parse.cpp
@@ -127,6 +127,7 @@ Parser::Parser(Js::ScriptContext* scriptContext, BOOL strictMode, PageAllocator 
     m_UsesArgumentsAtGlobal = false;
     m_currentNodeFunc = nullptr;
     m_currentNodeDeferredFunc = nullptr;
+    m_currentNodeNonNLambaFunc = nullptr;
     m_currentNodeProg = nullptr;
     m_currDeferredStub = nullptr;
     m_pstmtCur = nullptr;
@@ -1894,6 +1895,10 @@ ParseNode *Parser::GetCurrentFunctionNode()
     }
 }
 
+ParseNode *Parser::GetCurrentNonLamdaFunctionNode()
+{
+    return m_currentNodeNonNLambaFunc;
+}
 void Parser::RegisterRegexPattern(UnifiedRegex::RegexPattern *const regexPattern)
 {
     Assert(regexPattern);
@@ -2058,6 +2063,23 @@ ParseNodePtr Parser::ParseMetaProperty(tokens metaParentKeyword, charcount_t ich
 {
     AssertMsg(metaParentKeyword == tkNEW, "Only supported for tkNEW parent keywords");
     AssertMsg(this->m_token.tk == tkDot, "We must be currently sitting on the dot after the parent keyword");
+    
+    PnBlock currBlock = GetCurrentBlockInfo()->pnodeBlock->sxBlock;
+    switch (currBlock.scope->GetScopeType())
+    {
+        case ScopeType_FunctionBody:
+        {
+            ParseNode* currentFunction = GetCurrentFunctionNode();
+            Assert(currentFunction->nop == knopFncDecl);
+            if (!currentFunction->sxFnc.IsLambda() || GetCurrentNonLamdaFunctionNode() != nullptr)
+            {
+                break;
+            }
+            
+        }
+        default:  
+            Error(ERRInvalidNewTarget);
+    }
 
     m_pscan->Scan();
     
@@ -3688,11 +3710,13 @@ ParseNodePtr Parser::ParseFncDecl(ushort flags, LPCOLESTR pNameHint, const bool 
     ParseNodePtr pnodeFnc = nullptr;
     ParseNodePtr *ppnodeVarSave = nullptr;
     ParseNodePtr pnodeFncSave = nullptr;
+    ParseNodePtr pnodeFncSaveNonLambda = nullptr;
     ParseNodePtr pnodeFncBlockScope = nullptr;
     ParseNodePtr *ppnodeScopeSave = nullptr;
     ParseNodePtr *ppnodeExprScopeSave = nullptr;
     bool funcHasName = false;
     bool fDeclaration = flags & fFncDeclaration;
+    bool fLambda = (flags & fFncLambda) != 0;
     charcount_t ichMin = this->m_pscan->IchMinTok();
     bool wasInDeferredNestedFunc = false;
     CatchPidRefList *catchPidRefList = nullptr;
@@ -3787,6 +3811,12 @@ ParseNodePtr Parser::ParseFncDecl(ushort flags, LPCOLESTR pNameHint, const bool 
         pnodeFncSave = m_currentNodeFunc;
         m_currentNodeFunc = pnodeFnc;
 
+        if (!fLambda)
+        {
+            pnodeFncSaveNonLambda = m_currentNodeNonNLambaFunc;
+            m_currentNodeNonNLambaFunc = pnodeFnc;
+        }
+
         m_pnestedCount = &pnodeFnc->sxFnc.nestedCount;
 
         catchPidRefList = this->GetCatchPidRefList();
@@ -3830,7 +3860,6 @@ ParseNodePtr Parser::ParseFncDecl(ushort flags, LPCOLESTR pNameHint, const bool 
         {
             pnodeFncSave = m_currentNodeDeferredFunc;
             m_currentNodeDeferredFunc = pnodeFnc;
-
             m_pnestedCount = &pnodeFnc->sxFnc.nestedCount;
         }
         else
@@ -3842,7 +3871,7 @@ ParseNodePtr Parser::ParseFncDecl(ushort flags, LPCOLESTR pNameHint, const bool 
     if (buildAST || BindDeferredPidRefs())
     {
         pnodeFnc->sxFnc.SetIsAsync((flags & fFncAsync) != 0);
-        pnodeFnc->sxFnc.SetIsLambda((flags & fFncLambda) != 0);
+        pnodeFnc->sxFnc.SetIsLambda(fLambda);
         pnodeFnc->sxFnc.SetIsMethod((flags & fFncMethod) != 0);
         pnodeFnc->sxFnc.SetIsClassMember((flags & fFncClassMember) != 0);
     }
@@ -3868,6 +3897,12 @@ ParseNodePtr Parser::ParseFncDecl(ushort flags, LPCOLESTR pNameHint, const bool 
 
             m_currentNodeFunc = pnodeFncSave;
             m_pCurrentAstSize = pAstSizeSave;
+
+            if (!fLambda)
+            {
+                Assert(pnodeFnc == m_currentNodeNonNLambaFunc);
+                m_currentNodeNonNLambaFunc = pnodeFncSaveNonLambda;
+            }
         }
         else
         {
