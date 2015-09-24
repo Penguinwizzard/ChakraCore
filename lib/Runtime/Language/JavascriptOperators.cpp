ebc968878b1e061dda1257e944ef13f1b0ac0ae3
Yong Qu <yongqu@microsoft.com>
2015-09-24 14:42:36 -0700

    build break fix

--- lib/Runtime/Language/JavascriptOperators.cpp
+++ lib/Runtime/Language/JavascriptOperators.cpp
@@ -4146,14 +4146,6 @@ CommonNumber:
             }
             else
             {
-<<<<<<< 3aaf2ba70c3aa32a1ba134ea818952ab95f01cfb
-                if (!propertyRecord)
-                {
-                    //Lets get the propertyRecord from the propertyString
-                    scriptContext->GetOrAddPropertyRecord(propertyNameString->GetString(), propertyNameString->GetLength(), &propertyRecord);
-                }
-=======
->>>>>>> simple cleanup
                 return  JavascriptOperators::SetPropertyOnTaggedNumber(receiver, object, propertyRecord->GetPropertyId(), value, scriptContext, flags);
             }
         }
