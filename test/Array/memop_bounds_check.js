e2338a0ee663eb5cc25222b3ce56e29208ef26cc
Ian Halliday <ianhall@microsoft.com>
2015-10-15 19:32:41 -0700

    Add more copyright headers and move more 3rd party tests
    Move more 3rd party code from core\test\utf8 back to full repo under
    unittest.
    
    Add copyright license header to recently added tests.

--- test/Array/memop_bounds_check.js
+++ test/Array/memop_bounds_check.js
@@ -1,3 +1,8 @@
+//-------------------------------------------------------------------------------------------------------
+// Copyright (C) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
+//-------------------------------------------------------------------------------------------------------
+
 function foo() {
   const size = 100;
   let a = new Array(size);
