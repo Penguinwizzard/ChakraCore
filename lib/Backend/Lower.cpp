6c6f90bf9c1f804f8ea5dceba7d47dd4b514b599
Louis Lafreniere <louisl@microsoft.com>
2015-11-02 15:21:56 -0800

    Deleted useless #pragma warning

--- lib/Backend/Lower.cpp
+++ lib/Backend/Lower.cpp
@@ -11,11 +11,6 @@
 #include "RegexCommon.h"
 #include "RegexPattern.h"
 
-#ifdef _M_X64_OR_ARM64
-// TODO: Clean this warning up
-#pragma warning(disable:4267) // 'var' : conversion from 'size_t' to 'type', possible loss of data
-#endif
-
 #include "ExternalLowerer.h"
 
 ///----------------------------------------------------------------------------
