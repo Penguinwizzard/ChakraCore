aad1d42ec9a27614015c933267edc7ed05612c59
Nagy Mostafa <b-namost@microsoft.com>
2015-11-23 17:06:16 -0800

    Opcodes and opcode->sig maps

--- lib/WasmReader/WasmBinaryReader.cpp
+++ lib/WasmReader/WasmBinaryReader.cpp
@@ -10,35 +10,77 @@
 namespace Wasm
 {
 
-    WasmBinaryReader::WasmBinaryReader(PageAllocator * alloc, byte* source, size_t length) :
-        m_alloc(L"WasmBinaryDecoder", alloc, Js::Throw::OutOfMemory),
+    namespace Binary
     {
-        m_context.offset = 0;
-        m_context.source = source;
-        m_context.length = length;
-        m_start = source;
-        m_end = source + length;
-    }
+        namespace WasmTypes
+        {
 
-    WasmOp
-    WasmBinaryReader::ReadFromScript()
-    {
-            // One module per file
-            return wnMODULE;
-    }
+                Signature::Signature() : args(nullptr), retType(LocalType::Limit), argCount(-1){}
+                Signature::Signature(ArenaAllocator *alloc, uint count, ...)
+                {
+                    va_list arguments;
+                    va_start(arguments, count);
+                
+                    assert(count > 0);
+                    argCount = count - 1;
+                    retType = va_arg(arguments, LocalType);
+                    args = AnewArray(alloc, LocalType, this.argCount);
+                    for (uint i = 0; i < argCount; i++)
+                    {
+                        args[i] = va_arg(arguments, LocalType);
+                    }
+                    va_end(arguments);
+                }
+        } // namespace WasmTypes
+    
+        WasmBinaryReader::WasmBinaryReader(PageAllocator * alloc, byte* source, size_t length) :
+            m_alloc(L"WasmBinaryDecoder", alloc, Js::Throw::OutOfMemory)
+        {
+            m_context.offset = 0;
+            m_context.source = source;
+            m_context.length = length;
+            m_start = source;
+            m_end = source + length;
 
-    WasmOp
-    WasmBinaryReader::ReadFromModule()
-    {
+            // initialize Op Signature Table
+            {
+#define WASM_SIGNATURE(id, count, ...) \
+    AssertMsg(count >= 0 && count <= 3, "Invalid count for op signature"); \
+    AssertMsg(id >= 0 && id < WasmTypes::OpSignatureId::Limit, "Invalid signature ID for op"); \
+    opSignatureTable[id] = WasmTypes::Signature(&m_alloc, count, __VA_ARGS__);
 
-    }
+#include "WasmBinaryOpcodes.h";
+           }
 
-    WasmOp
-    WasmBinaryReader::ReadExpr()
-    {
-    }
+           // initialize opcode to op signature table
+            {
+#define WASM_OPCODE(opname, opcode, token, sig) \
+    opSignature[OpCodes::opname] = sig;
+            }
 
-   
+       }
+
+       WasmOp
+       WasmBinaryReader::ReadFromScript()
+       {
+               // One module per binary file
+               return wnMODULE;
+       }
+
+       WasmOp
+       WasmBinaryReader::ReadFromModule()
+       {
+           // Read all module sections.
+
+
+       }
+
+       WasmOp
+       WasmBinaryReader::ReadExpr()
+       {
+       }
+
+    } // namespace Binary
 } // namespace Wasm
 
 #endif // ENABLE_WASM
--- lib/WasmReader/WasmBinaryReader/WasmBinaryReader.h
+++ lib/WasmReader/WasmBinaryReader/WasmBinaryReader.h
@@ -7,73 +7,97 @@
 
 namespace Wasm
 {
-    struct DecoderContext
+    namespace Binary
     {
-        byte* source;
-        size_t offset;
-        size_t length;
-    };
-
-    namespace WasmTypes
-    {
-        // local types in binary format
-        enum BinLocalType {
-            Stmt = 0,  // void
-            I32 = 1,
-            I64 = 2,
-            F32 = 3,
-            F64 = 4,
-            Limit
-        };
-
-        // memory and global types in binary format
-        enum BinMemType {
-            I8 = 0,
-            U8 = 1,
-            I16 = 2,
-            U16 = 3,
-            I32 = 4,
-            U32 = 5,
-            I64 = 6,
-            U64 = 7,
-            F32 = 8,
-            F64 = 9,
-            Limit
-        };
-
-        struct OpSignature
+        struct DecoderContext
         {
-            BinLocalType retType;
-            BinLocalType argTypes[2];
+            byte* source;
+            size_t offset;
+            size_t length;
         };
+
+        namespace WasmTypes
+        {
+            // based on binary format encoding values
+            enum LocalType {
+                Stmt = 0,  // void
+                I32 = 1,
+                I64 = 2,
+                F32 = 3,
+                F64 = 4,
+                Limit
+            };
+
+            // memory and global types in binary format
+            enum MemType {
+                I8 = 0,
+                U8 = 1,
+                I16 = 2,
+                U16 = 3,
+                I32 = 4,
+                U32 = 5,
+                I64 = 6,
+                U64 = 7,
+                F32 = 8,
+                F64 = 9,
+                Limit
+            };
+
+            // for functions and opcodes
+            class Signature
+            {
+            private:
+                LocalType *args;
+                LocalType retType;
+                uint32 argCount;
+            public:
+                Signature();
+                Signature(ArenaAllocator *alloc, uint count, ...);
+            };
+
+            enum OpSignatureId
+            {
+#define WASM_SIGNATURE(id, ...) id,
+#include "WasmBinaryOpcodes.h"
+                Limit
+            };
+        }
+
+        // binary opcodes
+        enum OpCodes
+        {
+#define WASM_OPCODE(opname, opcode, token, sig) opname = opcode,
+#include "WasmBinaryOpcodes.h"
+            Limit
+        };
+
+        class WasmBinaryReader : public BaseWasmReader
+        {
+        public:
+            WasmBinaryReader(PageAllocator * alloc, byte* source, size_t length);
+
+            virtual WasmOp ReadFromScript() override;
+            virtual WasmOp ReadFromModule() override;
+            virtual WasmOp ReadExpr() override;
+
+            static void __declspec(noreturn) ThrowSyntaxError();
+
+        protected:
+            DecoderContext   m_context;
+
+
+        private:
+            ArenaAllocator      m_alloc;
+            uint m_funcNumber;
+            WasmFunctionInfo *  m_funcInfo;
+
+            byte *m_start, *m_end;
+            
+            WasmTypes::Signature opSignatureTable[WasmTypes::OpSignatureId::Limit];
+            WasmTypes::OpSignatureId opSignature[256];
+
+        };
+
     }
-
-    class Signature
-    {
-        Signature(WasmTypes::WasmType retType, WasmTypes::WasmType argTypes)
-    };
-
-    class WasmBinaryReader : public BaseWasmReader
-    {
-    public:
-        WasmBinaryReader(PageAllocator * alloc, byte* source, size_t length);
-
-        virtual WasmOp ReadFromScript() override;
-        virtual WasmOp ReadFromModule() override;
-        virtual WasmOp ReadExpr() override;
-
-        static void __declspec(noreturn) ThrowSyntaxError();
-
-    protected:
-        DecoderContext   m_context;
-        
-
-    private:
-        ArenaAllocator      m_alloc;
-        uint m_funcNumber;
-        WasmFunctionInfo *  m_funcInfo;
-
-        byte *m_start, *m_end;
-    };
 
 } // namespace Wasm
