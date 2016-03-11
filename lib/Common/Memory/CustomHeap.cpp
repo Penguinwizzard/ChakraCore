Penguinwizzard/ChakraCore

commit 887121ca256fc0aef45c317bae19ba4148d29ceb

Add SD changes from inetcore CL#1652591 to CL#1656106 and onecoreuap CL#47368 to CL#47368.
Change 47368 on 2016/02/27 17:43:23 by REDMOND\thomasmo@TH-R620V2-35-160226163352

        BUILD: Fix bad merge in LowererMD::SaveDoubleToVar

Change 1656106 on 2016/02/27 16:04:20 by NTDEV\wincbld@AISVM649-160205140428

        FI: BRANCH: FROM rs1_onecore_mqedge1_stage TO rs1_onecore_mqedge1_stage_dev3 @2016/02/26:08:21:01 [REFACTORED]

Change 1655925 on 2016/02/26 21:00:51 by NTDEV\wincbld@AISVM649-160205140428

        FI: BRANCH: FROM rs1_onecore_mqedge1_stage TO rs1_onecore_mqedge1_stage_dev3 @2016/02/26:08:21:01

Change 1654392 on 2016/02/23 17:14:57 by NTDEV\yongqu@IEDRTHOSTH-RM13

        FW: FW: merge minimal change from git to SD to enable crawler for tonight's build.
        FW: merge minimal change from git to SD to enable crawler for tonight's build.
        IECAT #: 81758
        Code Reviewer: saravind

Change 1653818 on 2016/02/22 15:35:39 by REDMOND\saravind@RSK-DEV-160216102643

        BUILD:Fixing Bad merge

Change 1652912 on 2016/02/18 18:57:34 by NTDEV\tawoll@TAWOLL-PRIME-160201140400

        FI: Fix build break after FI

Change 1652591 on 2016/02/17 21:23:44 by NTDEV\wincbld@AISVM649-160205140428

        FI: BRANCH: FROM rs1_onecore_mqedge1_stage TO rs1_onecore_mqedge1_stage_dev3 @2016/02/17:14:52:05

lib/Common/Memory/CustomHeap.cpp
@@ -21,7 +21,7 @@ namespace CustomHeap
 #pragma region "Constructor and Destructor"
 
 Heap::Heap(ArenaAllocator * alloc, CodePageAllocators * codePageAllocators):
-    auxiliaryAllocator(alloc),
+    auxilliaryAllocator(alloc),
     codePageAllocators(codePageAllocators),
     lastSecondaryAllocStateChangedCount(0)
 #if DBG_DUMP
@@ -408,7 +408,7 @@ Allocation* Heap::AllocLargeObject(size_t bytes, ushort pdataCount, ushort xdata
     }
 
 
-    Allocation* allocation = this->largeObjectAllocations.PrependNode(this->auxiliaryAllocator);
+    Allocation* allocation = this->largeObjectAllocations.PrependNode(this->auxilliaryAllocator);
     if (allocation == nullptr)
     {
         CodePageAllocators::AutoLock autoLock(this->codePageAllocators);
@@ -444,7 +444,7 @@ void Heap::FreeDecommittedLargeObjects()
 
         this->codePageAllocators->ReleaseDecommitted(allocation.address, allocation.GetPageCount(), allocation.largeObjectAllocation.segment);
 
-        largeObjectIter.RemoveCurrent(this->auxiliaryAllocator);
+        largeObjectIter.RemoveCurrent(this->auxilliaryAllocator);
     }
     NEXT_DLISTBASE_ENTRY_EDITING;
 }
@@ -485,7 +485,7 @@ void Heap::FreeLargeObjects()
 #endif
         this->codePageAllocators->Release(allocation.address, allocation.GetPageCount(), allocation.largeObjectAllocation.segment);
 
-        largeObjectIter.RemoveCurrent(this->auxiliaryAllocator);
+            largeObjectIter.RemoveCurrent(this->auxilliaryAllocator);
     }
     NEXT_DLISTBASE_ENTRY_EDITING;
 }
@@ -500,7 +500,7 @@ void Heap::FreeLargeObject(Allocation* allocation)
 #endif
     this->codePageAllocators->Release(allocation->address, allocation->GetPageCount(), allocation->largeObjectAllocation.segment);
 
-    this->largeObjectAllocations.RemoveElement(this->auxiliaryAllocator, allocation);
+    this->largeObjectAllocations.RemoveElement(this->auxilliaryAllocator, allocation);
 }
 
 #pragma endregion
@@ -509,7 +509,7 @@ void Heap::FreeLargeObject(Allocation* allocation)
 
 bool Heap::AllocInPage(Page* page, size_t bytes, ushort pdataCount, ushort xdataSize, Allocation ** allocationOut)
 {
-    Allocation * allocation = AnewNoThrowStruct(this->auxiliaryAllocator, Allocation);
+    Allocation * allocation = AnewNoThrowStruct(this->auxilliaryAllocator, Allocation);
     if (allocation == nullptr)
     {
         return true;
@@ -529,7 +529,7 @@ bool Heap::AllocInPage(Page* page, size_t bytes, ushort pdataCount, ushort xdata
         CodePageAllocators::AutoLock autoLock(this->codePageAllocators);
         if (this->ShouldBeInFullList(page))
         {
-            Adelete(this->auxiliaryAllocator, allocation);
+            Adelete(this->auxilliaryAllocator, allocation);
             // If we run out of XData space with the segment, move the page to the full page list, and return false to try the next page.
             BucketId bucket = page->currentBucket;
             VerboseHeapTrace(_u("Moving page from bucket %d to full list\n"), bucket);
@@ -542,7 +542,7 @@ bool Heap::AllocInPage(Page* page, size_t bytes, ushort pdataCount, ushort xdata
 
         if (!this->codePageAllocators->AllocSecondary(page->segment, (ULONG_PTR)address, bytes, pdataCount, xdataSize, &xdata))
         {
-            Adelete(this->auxiliaryAllocator, allocation);
+            Adelete(this->auxilliaryAllocator, allocation);
             return true;
         }
     }
@@ -621,7 +621,7 @@ Page* Heap::AllocNewPage(BucketId bucket, bool canAllocInPreReservedHeapPageSegm
 
     // Switch to allocating on a list of pages so we can do leak tracking later
     VerboseHeapTrace(_u("Allocing new page in bucket %d\n"), bucket);
-    Page* page = this->buckets[bucket].PrependNode(this->auxiliaryAllocator, address, pageSegment, bucket);
+    Page* page = this->buckets[bucket].PrependNode(this->auxilliaryAllocator, address, pageSegment, bucket);
 
     if (page == nullptr)
     {
@@ -756,7 +756,7 @@ bool Heap::FreeAllocation(Allocation* object)
 
             void* pageAddress = page->address;
 
-            this->fullPages[page->currentBucket].RemoveElement(this->auxiliaryAllocator, page);            
+            this->fullPages[page->currentBucket].RemoveElement(this->auxilliaryAllocator, page);            
 
             // The page is not in any bucket- just update the stats, free the allocation
             // and dump the page- we don't need to update free object size since the object
@@ -764,7 +764,7 @@ bool Heap::FreeAllocation(Allocation* object)
 #if DBG_DUMP
             this->totalAllocationSize -= pageSize;
 #endif
-            this->auxiliaryAllocator->Free(object, sizeof(Allocation));
+            this->auxilliaryAllocator->Free(object, sizeof(Allocation));
             {
                 CodePageAllocators::AutoLock autoLock(this->codePageAllocators);
                 this->codePageAllocators->ReleasePages(pageAddress, 1, segment);
@@ -807,11 +807,11 @@ bool Heap::FreeAllocation(Allocation* object)
     this->freesSinceLastCompact += object->size;
 #endif
 
-    this->auxiliaryAllocator->Free(object, sizeof(Allocation));
+    this->auxilliaryAllocator->Free(object, sizeof(Allocation));
 
     if (page->IsEmpty())
     {
-        this->buckets[page->currentBucket].RemoveElement(this->auxiliaryAllocator, page);
+        this->buckets[page->currentBucket].RemoveElement(this->auxilliaryAllocator, page);
         return false;
     }
     else // after freeing part of the page, the page should be in PAGE_EXECUTE_READWRITE protection, and turning to PAGE_EXECUTE (always with TARGETS_NO_UPDATE state)
@@ -840,7 +840,7 @@ void Heap::FreeDecommittedBuckets()
     FOREACH_DLISTBASE_ENTRY_EDITING(Page, page, &this->decommittedPages, iter)
     {
         this->codePageAllocators->TrackDecommittedPages(page.address, 1, page.segment);
-        iter.RemoveCurrent(this->auxiliaryAllocator);
+        iter.RemoveCurrent(this->auxilliaryAllocator);
     }
     NEXT_DLISTBASE_ENTRY_EDITING;
 }
@@ -872,7 +872,7 @@ void Heap::FreeBucket(DListBase<Page>* bucket, bool freeOnlyEmptyPages)
         if (!freeOnlyEmptyPages || page.IsEmpty())
         {
             FreePage(&page);
-            pageIter.RemoveCurrent(this->auxiliaryAllocator);
+            pageIter.RemoveCurrent(this->auxilliaryAllocator);
         }
     }
     NEXT_DLISTBASE_ENTRY_EDITING;

lib/Common/Memory/CustomHeap.h
@@ -544,7 +544,7 @@ private:
      * Allocator stuff
      */
     CodePageAllocators *                              codePageAllocators;
-    ArenaAllocator*                                   auxiliaryAllocator;
+    ArenaAllocator*                                   auxilliaryAllocator;
 
     /*
      * Various tracking lists

lib/Runtime/Library/JavascriptRegularExpression.cpp
@@ -754,7 +754,7 @@ namespace Js
 
         CHAKRATEL_LANGSTATS_INC_BUILTINCOUNT(RegexSymbolReplaceCount);
 
-        PCWSTR varName = _u("RegExp.prototype[Symbol.replace]");
+        PCWSTR varName = L"RegExp.prototype[Symbol.replace]";
 
         RecyclableObject* thisObj = GetThisObject(args, varName, scriptContext);
         JavascriptString* string = GetFirstStringArg(args, scriptContext);

lib/Runtime/Library/JavascriptString.cpp
@@ -1557,7 +1557,7 @@ case_2:
 
         PCWSTR const varName = _u("String.prototype.replace");
 
-        AUTO_TAG_NATIVE_LIBRARY_ENTRY(function, callInfo, _u("String.prototype.replace"));
+        AUTO_TAG_NATIVE_LIBRARY_ENTRY(function, callInfo, varName);
 
         Assert(!(callInfo.Flags & CallFlags_New));
 

lib/Runtime/Library/RegexHelper.cpp
@@ -663,7 +663,7 @@ namespace Js
         , int numGroups
         , GroupFn getGroup
         , JavascriptString* input
-        , const char16* matchedString
+        , const wchar_t* matchedString
         , UnifiedRegex::GroupInfo match
         , JavascriptString* replace
         , int substitutions
@@ -794,8 +794,8 @@ namespace Js
         else
         {
             PCWSTR varName = scriptConfig->IsES6RegExSymbolsEnabled()
-                ? _u("RegExp.prototype[Symbol.replace]")
-                : _u("String.prototype.replace");
+                ? L"RegExp.prototype[Symbol.replace]"
+                : L"String.prototype.replace";
             JavascriptRegExp* regularExpression = JavascriptRegExp::ToRegExp(thisObj, varName, scriptContext);
             return RegexEs5ReplaceImpl(scriptContext, regularExpression, input, replace, noResult);
         }
@@ -814,7 +814,7 @@ namespace Js
     Var RegexHelper::RegexEs6ReplaceImpl(ScriptContext* scriptContext, RecyclableObject* thisObj, JavascriptString* input, JavascriptString* replace, bool noResult)
     {
         auto appendReplacement = [&](
-            CompoundString::Builder<64 * sizeof(void *) / sizeof(char16)>& resultBuilder,
+            CompoundString::Builder<64 * sizeof(void *) / sizeof(wchar_t)>& resultBuilder,
             ArenaAllocator* tempAlloc,
             JavascriptString* matchStr,
             int numberOfCaptures,
@@ -850,7 +850,7 @@ namespace Js
     Var RegexHelper::RegexEs6ReplaceImpl(ScriptContext* scriptContext, RecyclableObject* thisObj, JavascriptString* input, JavascriptFunction* replaceFn)
     {
         auto appendReplacement = [&](
-            CompoundString::Builder<64 * sizeof(void *) / sizeof(char16)>& resultBuilder,
+            CompoundString::Builder<64 * sizeof(void *) / sizeof(wchar_t)>& resultBuilder,
             ArenaAllocator* tempAlloc,
             JavascriptString* matchStr,
             int numberOfCaptures,
@@ -905,14 +905,14 @@ namespace Js
 
         JavascriptString* accumulatedResult = nullptr;
 
-        BEGIN_TEMP_ALLOCATOR(tempAlloc, scriptContext, _u("RegexHelper"))
+        BEGIN_TEMP_ALLOCATOR(tempAlloc, scriptContext, L"RegexHelper")
         {
             JsUtil::List<RecyclableObject*, ArenaAllocator>* results =
                 JsUtil::List<RecyclableObject*, ArenaAllocator>::New(tempAlloc);
 
             while (true)
             {
-                PCWSTR varName = _u("RegExp.prototype[Symbol.replace]");
+                PCWSTR varName = L"RegExp.prototype[Symbol.replace]";
                 Var result = JavascriptRegExp::CallExec(thisObj, input, varName, scriptContext);
                 if (JavascriptOperators::IsNull(result))
                 {
@@ -932,7 +932,7 @@ namespace Js
                 AdvanceLastIndex(thisObj, input, matchStr, unicode, scriptContext);
             }
 
-            CompoundString::Builder<64 * sizeof(void *) / sizeof(char16)> accumulatedResultBuilder(scriptContext);
+            CompoundString::Builder<64 * sizeof(void *) / sizeof(wchar_t)> accumulatedResultBuilder(scriptContext);
             CharCount inputLength = input->GetLength();
             CharCount nextSourcePosition = 0;
 
@@ -1073,7 +1073,7 @@ namespace Js
                     auto getGroup = [&](int captureIndex, Var nonMatchValue) {
                         return GetGroup(scriptContext, pattern, input, nonMatchValue, captureIndex);
                     };
-                    const char16* matchedString = inputStr + lastActualMatch.offset;
+                    const wchar_t* matchedString = inputStr + lastActualMatch.offset;
                     ReplaceFormatString(scriptContext, pattern->NumGroups(), getGroup, input, matchedString, lastActualMatch, replace, substitutions, substitutionOffsets, concatenated);
                 }
                 else
@@ -1147,8 +1147,8 @@ namespace Js
         else
         {
             PCWSTR varName = scriptConfig->IsES6RegExSymbolsEnabled()
-                ? _u("RegExp.prototype[Symbol.replace]")
-                : _u("String.prototype.replace");
+                ? L"RegExp.prototype[Symbol.replace]"
+                : L"String.prototype.replace";
             JavascriptRegExp* regularExpression = JavascriptRegExp::ToRegExp(thisObj, varName, scriptContext);
             return RegexEs5ReplaceImpl(scriptContext, regularExpression, input, replacefn);
         }

commit 97597f05ca85836b21ac7a2c6c1b8c68339370d8

Fix "auxiliary" typo introduced in 887121ca256fc0aef45c317bae19ba4148d29ceb

lib/Common/Memory/CustomHeap.cpp
@@ -21,7 +21,7 @@ namespace CustomHeap
 #pragma region "Constructor and Destructor"
 
 Heap::Heap(ArenaAllocator * alloc, CodePageAllocators * codePageAllocators):
-    auxilliaryAllocator(alloc),
+    auxiliaryAllocator(alloc),
     codePageAllocators(codePageAllocators),
     lastSecondaryAllocStateChangedCount(0)
 #if DBG_DUMP
@@ -408,7 +408,7 @@ Allocation* Heap::AllocLargeObject(size_t bytes, ushort pdataCount, ushort xdata
     }
 
 
-    Allocation* allocation = this->largeObjectAllocations.PrependNode(this->auxilliaryAllocator);
+    Allocation* allocation = this->largeObjectAllocations.PrependNode(this->auxiliaryAllocator);
     if (allocation == nullptr)
     {
         CodePageAllocators::AutoLock autoLock(this->codePageAllocators);
@@ -444,7 +444,7 @@ void Heap::FreeDecommittedLargeObjects()
 
         this->codePageAllocators->ReleaseDecommitted(allocation.address, allocation.GetPageCount(), allocation.largeObjectAllocation.segment);
 
-        largeObjectIter.RemoveCurrent(this->auxilliaryAllocator);
+        largeObjectIter.RemoveCurrent(this->auxiliaryAllocator);
     }
     NEXT_DLISTBASE_ENTRY_EDITING;
 }
@@ -485,7 +485,7 @@ void Heap::FreeLargeObjects()
 #endif
         this->codePageAllocators->Release(allocation.address, allocation.GetPageCount(), allocation.largeObjectAllocation.segment);
 
-            largeObjectIter.RemoveCurrent(this->auxilliaryAllocator);
+            largeObjectIter.RemoveCurrent(this->auxiliaryAllocator);
     }
     NEXT_DLISTBASE_ENTRY_EDITING;
 }
@@ -500,7 +500,7 @@ void Heap::FreeLargeObject(Allocation* allocation)
 #endif
     this->codePageAllocators->Release(allocation->address, allocation->GetPageCount(), allocation->largeObjectAllocation.segment);
 
-    this->largeObjectAllocations.RemoveElement(this->auxilliaryAllocator, allocation);
+    this->largeObjectAllocations.RemoveElement(this->auxiliaryAllocator, allocation);
 }
 
 #pragma endregion
@@ -509,7 +509,7 @@ void Heap::FreeLargeObject(Allocation* allocation)
 
 bool Heap::AllocInPage(Page* page, size_t bytes, ushort pdataCount, ushort xdataSize, Allocation ** allocationOut)
 {
-    Allocation * allocation = AnewNoThrowStruct(this->auxilliaryAllocator, Allocation);
+    Allocation * allocation = AnewNoThrowStruct(this->auxiliaryAllocator, Allocation);
     if (allocation == nullptr)
     {
         return true;
@@ -529,7 +529,7 @@ bool Heap::AllocInPage(Page* page, size_t bytes, ushort pdataCount, ushort xdata
         CodePageAllocators::AutoLock autoLock(this->codePageAllocators);
         if (this->ShouldBeInFullList(page))
         {
-            Adelete(this->auxilliaryAllocator, allocation);
+            Adelete(this->auxiliaryAllocator, allocation);
             // If we run out of XData space with the segment, move the page to the full page list, and return false to try the next page.
             BucketId bucket = page->currentBucket;
             VerboseHeapTrace(_u("Moving page from bucket %d to full list\n"), bucket);
@@ -542,7 +542,7 @@ bool Heap::AllocInPage(Page* page, size_t bytes, ushort pdataCount, ushort xdata
 
         if (!this->codePageAllocators->AllocSecondary(page->segment, (ULONG_PTR)address, bytes, pdataCount, xdataSize, &xdata))
         {
-            Adelete(this->auxilliaryAllocator, allocation);
+            Adelete(this->auxiliaryAllocator, allocation);
             return true;
         }
     }
@@ -621,7 +621,7 @@ Page* Heap::AllocNewPage(BucketId bucket, bool canAllocInPreReservedHeapPageSegm
 
     // Switch to allocating on a list of pages so we can do leak tracking later
     VerboseHeapTrace(_u("Allocing new page in bucket %d\n"), bucket);
-    Page* page = this->buckets[bucket].PrependNode(this->auxilliaryAllocator, address, pageSegment, bucket);
+    Page* page = this->buckets[bucket].PrependNode(this->auxiliaryAllocator, address, pageSegment, bucket);
 
     if (page == nullptr)
     {
@@ -756,7 +756,7 @@ bool Heap::FreeAllocation(Allocation* object)
 
             void* pageAddress = page->address;
 
-            this->fullPages[page->currentBucket].RemoveElement(this->auxilliaryAllocator, page);            
+            this->fullPages[page->currentBucket].RemoveElement(this->auxiliaryAllocator, page);            
 
             // The page is not in any bucket- just update the stats, free the allocation
             // and dump the page- we don't need to update free object size since the object
@@ -764,7 +764,7 @@ bool Heap::FreeAllocation(Allocation* object)
 #if DBG_DUMP
             this->totalAllocationSize -= pageSize;
 #endif
-            this->auxilliaryAllocator->Free(object, sizeof(Allocation));
+            this->auxiliaryAllocator->Free(object, sizeof(Allocation));
             {
                 CodePageAllocators::AutoLock autoLock(this->codePageAllocators);
                 this->codePageAllocators->ReleasePages(pageAddress, 1, segment);
@@ -807,11 +807,11 @@ bool Heap::FreeAllocation(Allocation* object)
     this->freesSinceLastCompact += object->size;
 #endif
 
-    this->auxilliaryAllocator->Free(object, sizeof(Allocation));
+    this->auxiliaryAllocator->Free(object, sizeof(Allocation));
 
     if (page->IsEmpty())
     {
-        this->buckets[page->currentBucket].RemoveElement(this->auxilliaryAllocator, page);
+        this->buckets[page->currentBucket].RemoveElement(this->auxiliaryAllocator, page);
         return false;
     }
     else // after freeing part of the page, the page should be in PAGE_EXECUTE_READWRITE protection, and turning to PAGE_EXECUTE (always with TARGETS_NO_UPDATE state)
@@ -840,7 +840,7 @@ void Heap::FreeDecommittedBuckets()
     FOREACH_DLISTBASE_ENTRY_EDITING(Page, page, &this->decommittedPages, iter)
     {
         this->codePageAllocators->TrackDecommittedPages(page.address, 1, page.segment);
-        iter.RemoveCurrent(this->auxilliaryAllocator);
+        iter.RemoveCurrent(this->auxiliaryAllocator);
     }
     NEXT_DLISTBASE_ENTRY_EDITING;
 }
@@ -872,7 +872,7 @@ void Heap::FreeBucket(DListBase<Page>* bucket, bool freeOnlyEmptyPages)
         if (!freeOnlyEmptyPages || page.IsEmpty())
         {
             FreePage(&page);
-            pageIter.RemoveCurrent(this->auxilliaryAllocator);
+            pageIter.RemoveCurrent(this->auxiliaryAllocator);
         }
     }
     NEXT_DLISTBASE_ENTRY_EDITING;

lib/Common/Memory/CustomHeap.h
@@ -544,7 +544,7 @@ private:
      * Allocator stuff
      */
     CodePageAllocators *                              codePageAllocators;
-    ArenaAllocator*                                   auxilliaryAllocator;
+    ArenaAllocator*                                   auxiliaryAllocator;
 
     /*
      * Various tracking lists