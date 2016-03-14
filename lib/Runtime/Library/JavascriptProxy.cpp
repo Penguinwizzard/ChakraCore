9de6f9149b4d7004d10b7fd3f800c71120813f78
Yong Qu <yongqu@microsoft.com>
2016-03-13 17:18:22 -0700

    VSO 6363150: fix up getPrototypeOf handling for proxy We should use JavascriptObject version in proxy default handler to ensure all checks are properly done

--- lib/Runtime/Library/JavascriptProxy.cpp
+++ lib/Runtime/Library/JavascriptProxy.cpp
@@ -1298,7 +1298,7 @@ namespace Js
         Var getPrototypeOfResult;
         if (nullptr == getPrototypeOfMethod || GetScriptContext()->IsHeapEnumInProgress())
         {
-            return target->GetPrototype();
+            return RecyclableObject::FromVar(JavascriptObject::GetPrototypeOf(target, scriptContext));
         }
         CallInfo callInfo(CallFlags_Value, 2);
         Var varArgs[2];
