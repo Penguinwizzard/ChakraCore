3828c86fc86aee96ad7b4e52ed24b250225c9acf
Paul Leathers <pleath@microsoft.com>
2016-05-11 14:37:16 -0700

    Restructure the FunctionBody hierarchy so that FunctionInfo is a standalone proxy from which FunctionProxy does not inherit, and FunctionProxy is the basis for all the representations of user functions (FunctionBody, etc.). FunctionInfo still points to the FunctionProxy that implements the function, and FunctionProxy points to FunctionInfo. Do this to facilitate re-deferral and to maximize the memory benefit.

--- lib/Runtime/Runtime.h
+++ lib/Runtime/Runtime.h
@@ -238,6 +238,7 @@ namespace Js
     struct TickDelta;
     class ByteBlock;
     class FunctionInfo;
+    class FunctionProxy;
     class FunctionBody;
     class ParseableFunctionInfo;
     struct StatementLocation;
