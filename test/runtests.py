0d470220cec3dc16631c03bf9ca550136e4854fb
Jianchun Xu <jianxu@microsoft.com>
2016-05-16 14:48:49 -0700

    xplat: enhance runtests.py
    Support "pass" baselines
    Support tags, not_tags (excludes)

--- test/runtests.py
+++ test/runtests.py
@@ -4,6 +4,7 @@
 # Licensed under the MIT license. See LICENSE.txt file in the project root for full license information.
 #-------------------------------------------------------------------------------------------------------
 
+from __future__ import print_function
 import sys
 import os
 import subprocess as SP
@@ -34,6 +35,12 @@ parser.add_argument('-b', '--binary', metavar='bin', help='ch full path')
 parser.add_argument('-d', '--debug', action='store_true',
                     help='use debug build');
 parser.add_argument('-t', '--test', action='store_true', help='use test build')
+parser.add_argument('--include-slow', action='store_true',
+                    help='include slow tests')
+parser.add_argument('--only-slow', action='store_true',
+                    help='run only slow tests')
+parser.add_argument('--nightly', action='store_true',
+                    help='run as nightly tests')
 parser.add_argument('--x86', action='store_true', help='use x86 build')
 parser.add_argument('--x64', action='store_true', help='use x64 build')
 args = parser.parse_args()
@@ -64,6 +71,17 @@ if binary == None:
 if not os.path.isfile(binary):
     print('{} not found. Did you run ./build.sh already?'.format(binary))
     sys.exit(1)
+
+# global tags/not_tags
+tags = set()
+not_tags = set(['fail'])
+if args.only_slow:
+    tags.add('Slow')
+elif not args.include_slow:
+    not_tags.add('Slow')
+
+not_tags.add('exclude_nightly' if args.nightly else 'nightly')
+
 
 # records pass_count/fail_count
 class PassFailCount(object):
@@ -98,86 +116,123 @@ class TestResult(PassFailCount):
             r.pass_count += 1
             self.pass_count += 1
 
-test_result = TestResult()
 
+# test variants:
+#   interpreted: -maxInterpretCount:1 -maxSimpleJitRunCount:1 -bgjit-
+#   dynapogo: -forceNative -off:simpleJit -bgJitDelay:0
+class TestVariant(object):
+    def __init__(self, name, compile_flags=[]):
+        self.name = name
+        self.compile_flags = [
+            '-WERExceptionSupport', '-ExtendedErrorStackForTestHost'] \
+            + compile_flags
+        self.tags = tags.copy()
+        self.not_tags = not_tags.union(
+            ['{}_{}'.format(x, name) for x in 'fails','excludes'])
+        self.test_result = TestResult()
 
-def show_failed(filename, output, exit_code, expected_output):
-    print("\nFailed -> {}".format(filename))
-    if expected_output == None:
-        print("\nOutput:")
-        print("----------------------------")
-        print(output)
-        print("----------------------------")
-    else:
-        lst_output = output.split('\n')
-        lst_expected = expected_output.split('\n')
-        ln = min(len(lst_output), len(lst_expected))
-        for i in range(0, ln):
-            if lst_output[i] != lst_expected[i]:
-                print("Output: (at line " + str(i) + ")")
-                print("----------------------------")
-                print(lst_output[i])
-                print("----------------------------")
-                print("Expected Output:")
-                print("----------------------------")
-                print(lst_expected[i])
-                print("----------------------------")
-                break
+    def should_test(self, test):
+        tags = test.get('tags')
+        if tags:
+            tags = set(tags.split(','))
+            if not tags.isdisjoint(self.not_tags):
+                return False
+            if self.tags and not tags.issubset(self.tags):
+                return False
+        return True
 
-    print("exit code: {}".format(exit_code))
-    test_result.log(filename, fail=True)
+    def log_result(self, filename=None, folder=None, fail=False):
+        self.test_result.log(filename, folder, fail)
 
-def test_path(path):
-    if os.path.isfile(path):
-        folder, file = os.path.dirname(path), os.path.basename(path)
-    else:
-        folder, file = path, None
+    def show_failed(self, filename, output, exit_code, expected_output):
+        print("\nFailed -> {}".format(filename))
+        if expected_output == None:
+            print("\nOutput:")
+            print("----------------------------")
+            print(output)
+            print("----------------------------")
+        else:
+            lst_output = output.split('\n')
+            lst_expected = expected_output.split('\n')
+            ln = min(len(lst_output), len(lst_expected))
+            for i in range(0, ln):
+                if lst_output[i] != lst_expected[i]:
+                    print("Output: (at line " + str(i) + ")")
+                    print("----------------------------")
+                    print(lst_output[i])
+                    print("----------------------------")
+                    print("Expected Output:")
+                    print("----------------------------")
+                    print(lst_expected[i])
+                    print("----------------------------")
+                    break
 
-    tests = load_tests(folder, file)
-    if len(tests) == 0:
-        return
+        print("exit code: {}".format(exit_code))
+        self.log_result(filename, fail=True)
 
-    print("Testing -> " + os.path.basename(folder))
-    for test in tests:
-        test_one(folder, test)
+    def test_one(self, folder, test):
+        js_file = os.path.join(folder, test['files'])
+        js_output = ""
 
-def test_one(folder, test):
-    js_file = os.path.join(folder, test['files'])
-    js_output = ""
+        working_path = os.path.dirname(js_file)
+        file_name = os.path.basename(js_file)
 
-    flags = test.get('compile-flags')
+        flags = test.get('compile-flags')
+        flags = self.compile_flags + (flags.split() if flags else [])
+        cmd = [binary] + flags + [file_name]
+        p = SP.Popen(cmd, stdout=SP.PIPE, stderr=SP.STDOUT, cwd=working_path)
+        js_output = p.communicate()[0].replace('\r','')
+        exit_code = p.wait()
 
-    working_path = os.path.dirname(js_file)
-    file_name = os.path.basename(js_file)
+        if exit_code != 0:
+            return self.show_failed(js_file, js_output, exit_code, None)
 
-    cmd = [binary, '-WERExceptionSupport', '-ExtendedErrorStackForTestHost'] \
-          + (flags.split() if flags else []) \
-          + [file_name]
+        if 'baseline' not in test:
+            # output lines must be 'pass' or 'passed' or empty
+            lines = (line.lower() for line in js_output.split('\n'))
+            if any(line != '' and line != 'pass' and line != 'passed'
+                    for line in lines):
+                return self.show_failed(js_file, js_output, exit_code, None)
+        else:
+            baseline = test.get('baseline')
+            if baseline:
+                # perform baseline comparison
+                baseline = os.path.join(working_path, baseline)
+                expected_output = None
+                with open(baseline, 'r') as bs_file:
+                    baseline_output = bs_file.read()
 
-    p = SP.Popen(cmd, stdout=SP.PIPE, stderr=SP.STDOUT, cwd=working_path)
-    js_output = p.communicate()[0].replace('\r','')
-    exit_code = p.wait()
+                # Cleanup carriage return
+                # todo: remove carriage return at the end of the line
+                #       or better fix ch to output same on all platforms
+                expected_output = re.sub('[\r]+\n', '\n', baseline_output)
 
-    if exit_code != 0:
-        return show_failed(js_file, js_output, exit_code, None)
-    else: #compare outputs
-        baseline = os.path.splitext(js_file)[0] + '.baseline'
-        if os.path.isfile(baseline):
-            expected_output = None
-            with open(baseline, 'r') as bs_file:
-                baseline_output = bs_file.read()
+                # todo: implement wild cards support
+                if expected_output != js_output:
+                    return self.show_failed(js_file, js_output, exit_code, expected_output)
 
-            # Cleanup carriage return
-            # todo: remove carriage return at the end of the line
-            #       or better fix ch to output same on all platforms
-            expected_output = re.sub('[\r]+\n', '\n', baseline_output)
+        print("\tPassed -> " + os.path.basename(js_file))
+        self.log_result(js_file)
 
-            # todo: implement wild cards support
-            if expected_output != js_output:
-                return show_failed(js_file, js_output, exit_code, expected_output)
+    def run(self, tests):
+        print('\n############# Starting {} variant #############'.format(
+            self.name))
+        if self.tags:
+            print('  tags: {}'.format(self.tags))
+        for x in self.not_tags:
+            print('  exclude: {}'.format(x))
+        print()
 
-    print("\tPassed -> " + os.path.basename(js_file))
-    test_result.log(folder=folder)
+        tests = [x for x in tests if self.should_test(x[1])]
+        for folder,test in tests:
+            self.test_one(folder, test)
+
+    def print_summary(self):
+        print('\n######## Logs for {} variant ########'.format(self.name))
+        for folder, result in sorted(self.test_result.folders.items()):
+            print('{}: {}'.format(folder, result))
+        print('Total: {}'.format(self.test_result))
+
 
 def load_tests(folder, file):
     try:
@@ -186,6 +241,12 @@ def load_tests(folder, file):
     except IOError:
         return []
 
+    def load_test(testXml):
+        test = dict()
+        for c in testXml.find('default'):
+            test[c.tag] = c.text
+        return test
+
     tests = [load_test(x) for x in xml]
     if file != None:
         tests = [x for x in tests if x['files'] == file]
@@ -193,31 +254,41 @@ def load_tests(folder, file):
             tests = [{'files':file}]
     return tests
 
-def load_test(testXml):
-    test = dict()
-    for c in testXml.find('default'):
-        test[c.tag] = c.text
-    return test
-
 def is_jsfile(path):
     return os.path.splitext(path)[1] == '.js'
 
 def main():
     # By default run all tests
     if len(args.folders) == 0:
-        args.folders = [os.path.join(test_root, x)
-                        for x in sorted(os.listdir(test_root))]
+        files = (os.path.join(test_root, x) for x in os.listdir(test_root))
+        args.folders = [f for f in sorted(files) if not os.path.isfile(f)]
 
-    for folder in args.folders:
-        test_path(folder)
+    # load all tests
+    tests = []
+    for path in args.folders:
+        if os.path.isfile(path):
+            folder, file = os.path.dirname(path), os.path.basename(path)
+        else:
+            folder, file = path, None
+        tests += ((folder,test) for test in load_tests(folder, file))
 
-    print('\n')
-    print('============================')
-    for folder, result in sorted(test_result.folders.items()):
-        print('{}: {}'.format(folder, result))
-    print('============================')
-    print('Total: {}'.format(test_result))
-    print('Success!' if test_result.fail_count == 0 else 'Failed!')
+    # test variants
+    variants = [
+        TestVariant('interpreted', [
+            '-maxInterpretCount:1', '-maxSimpleJitRunCount:1', '-bgjit-'])
+    ]
+
+    # run each variant
+    for variant in variants:
+        variant.run(tests)
+
+    # print summary
+    for variant in variants:
+        variant.print_summary()
+
+    print()
+    failed = any(variant.test_result.fail_count > 0 for variant in variants)
+    print('Success!' if not failed else 'Failed!')
     return 0
 
 if __name__ == '__main__':
