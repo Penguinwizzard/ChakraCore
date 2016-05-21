9021adfe93dbfeb1c52d3d2fcf7f2e0272638534
Curtis Man <curtism@microsoft.com>
2016-05-20 20:36:51 -0700

    Fix leak of inline cache list when invalidating

--- lib/Runtime/Base/ThreadContext.cpp
+++ lib/Runtime/Base/ThreadContext.cpp
@@ -2696,7 +2696,7 @@ ThreadContext::InvalidateProtoInlineCaches(Js::PropertyId propertyId)
     InlineCacheList* inlineCacheList;
     if (protoInlineCacheByPropId.TryGetValueAndRemove(propertyId, &inlineCacheList))
     {
-        InvalidateInlineCacheList(inlineCacheList);
+        InvalidateAndDeleteInlineCacheList(inlineCacheList);
     }
 }
 
@@ -2713,12 +2713,12 @@ ThreadContext::InvalidateStoreFieldInlineCaches(Js::PropertyId propertyId)
     InlineCacheList* inlineCacheList;
     if (storeFieldInlineCacheByPropId.TryGetValueAndRemove(propertyId, &inlineCacheList))
     {
-        InvalidateInlineCacheList(inlineCacheList);
+        InvalidateAndDeleteInlineCacheList(inlineCacheList);
     }
 }
 
 void
-ThreadContext::InvalidateInlineCacheList(InlineCacheList* inlineCacheList)
+ThreadContext::InvalidateAndDeleteInlineCacheList(InlineCacheList* inlineCacheList)
 {
     Assert(inlineCacheList != nullptr);
 
@@ -2738,7 +2738,7 @@ ThreadContext::InvalidateInlineCacheList(InlineCacheList* inlineCacheList)
         }
     }
     NEXT_SLISTBASE_ENTRY;
-    inlineCacheList->Clear();
+    Adelete(&this->inlineCacheThreadInfoAllocator, inlineCacheList);
     this->registeredInlineCacheCount = this->registeredInlineCacheCount > cacheCount ? this->registeredInlineCacheCount - cacheCount : 0;
 }
 
@@ -3087,7 +3087,7 @@ ThreadContext::InvalidateAllProtoInlineCaches()
 {
     protoInlineCacheByPropId.Map([this](Js::PropertyId propertyId, InlineCacheList* inlineCacheList)
     {
-        InvalidateInlineCacheList(inlineCacheList);
+        InvalidateAndDeleteInlineCacheList(inlineCacheList);
     });
     protoInlineCacheByPropId.ResetNoDelete();
 }
@@ -3103,7 +3103,7 @@ ThreadContext::InvalidateAllStoreFieldInlineCaches()
 {
     storeFieldInlineCacheByPropId.Map([this](Js::PropertyId propertyId, InlineCacheList* inlineCacheList)
     {
-        InvalidateInlineCacheList(inlineCacheList);
+        InvalidateAndDeleteInlineCacheList(inlineCacheList);
     });
     storeFieldInlineCacheByPropId.ResetNoDelete();
 }
