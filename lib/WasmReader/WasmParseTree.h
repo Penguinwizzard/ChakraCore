b0e18263220cbe8d00d042f66bf4609c639430f5
George Kuan <commonlisp@gmail.com>
2016-09-16 11:36:08 -0700

    WasmReturnNode removal

--- lib/WasmReader/WasmParseTree.h
+++ lib/WasmReader/WasmParseTree.h
@@ -75,10 +75,6 @@ namespace Wasm
         uint32 defaultTarget;
     };
 
-    struct WasmReturnNode
-    {
-    };
-
     struct WasmCallNode
     {
         uint32 num; // function id
@@ -94,7 +90,6 @@ namespace Wasm
             WasmBrNode br;
             WasmBrTableNode brTable;
             WasmMemOpNode mem;
-            WasmReturnNode ret;
             WasmCallNode call;
         };
     };
