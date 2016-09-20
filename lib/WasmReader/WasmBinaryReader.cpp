da49d8fa395618a4f05d98e5dd2978c236af6cc6
George Kuan <commonlisp@gmail.com>
2016-09-20 11:14:54 -0700

    comment

--- lib/WasmReader/WasmBinaryReader.cpp
+++ lib/WasmReader/WasmBinaryReader.cpp
@@ -555,6 +555,7 @@ void WasmBinaryReader::ConstNode()
 bool
 WasmBinaryReader::EndOfFunc()
 {
+    // EndOfFunc() is checked before the trailing END opcode is read
     return m_funcState.count + 1 >= m_funcState.size;
 }
 
