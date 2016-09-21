d87c8e70a4386f970d965bc8acf5cac1c0a683ae
Satheesh Ravindranath <saravind@microsoft.com>
2016-09-21 16:35:01 -0700

    Correcting the version check for SSE4

--- lib/Backend/Encoder.cpp
+++ lib/Backend/Encoder.cpp
@@ -759,12 +759,12 @@ void Encoder::EnsureRelocEntryIntegrity(size_t newBufferStartAddress, size_t cod
 uint Encoder::CalculateCRC(uint bufferCRC, size_t data)
 {
 #if defined(_M_IX86)
-    if (AutoSystemInfo::Data.SSE4_1Available())
+    if (AutoSystemInfo::Data.SSE4_2Available())
     {
         return _mm_crc32_u32(bufferCRC, data);
     }
 #elif defined(_M_X64)
-    if (AutoSystemInfo::Data.SSE4_1Available())
+    if (AutoSystemInfo::Data.SSE4_2Available())
     {
         //CRC32 always returns a 32-bit result
         return (uint)_mm_crc32_u64(bufferCRC, data);
